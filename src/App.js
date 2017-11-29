642325b99109d83f16c77b7a16c6f81866100b90
JackiJang <t5istu01@students.oamk.fi>
2017-11-29 11:57:47 +0200

    Made basic functionality for dropdown menus not finished yet, is working with placeholder data for now

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
-import RegionsField from './components/DropDownRegional';
-import RegionsLvl from './components/DropDownRgnSlct';
-import Scenarios from './components/DropDownScnCol';
-import '../node_modules/react-bootstrap/dist/react-bootstrap.min.js';
-import DropdownButton from 'react-bootstrap';
-
+import React, { Component } from "react";
+import RegionsField from "./components/DropDownRegional";
+import RegionsLvl from "./components/DropDownRgnSlct";
+import Scenarios from "./components/DropDownScnCol";
+import "../node_modules/react-bootstrap/dist/react-bootstrap.min.js";
+import DropdownButton from "react-bootstrap";
 
 class App extends Component {
   render() {
@@ -13,12 +12,28 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Metsämittari</h1>
         </header>
-          <h3>Aluetaso: </h3>
-          <div className="col-md-4"><RegionsLvl /></div>,
-          <h3>Alue: </h3>
-          <div className="col-md-4"><RegionsField /></div>,
-          <h3>Skenaariot: </h3>
-          <div className="col-md-4"><Scenarios /></div>,
+        <div className="text-left">
+          <div className="grid">
+            <div className="row">
+              <h3>Aluetaso: </h3>
+              <div className="col-md-4">
+                <RegionsLvl />
+              </div>
+            </div>
+            <div className="row">
+              <h3>Alue: </h3>
+              <div className="col-md-4">
+                <RegionsField />
+              </div>
+            </div>
+            <div className="row">
+              <h3>Skenaariot: </h3>
+              <div className="col-md-4">
+                <Scenarios />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
